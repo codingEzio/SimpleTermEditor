a22f6c8f5b4fdff9a79949bc22e09ad4adc6a054
Nemo <assassinste@gmail.com>
2024-09-17 16:45:37 +0800

    feat: Editor WIP core logic for the main program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
-﻿namespace SimpleTermEditor;
+﻿using SimpleTermEditor.Utility;
+
+namespace SimpleTermEditor;
 
 public class Program
 {
-    public static void Main(string[] args)
+    /// <summary>
+    ///  A simple editor that supports basic text editing and navigation.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Either start a new file or open an existing file
+        Editor editor = new Editor();
+        if (args.Length > 0)
+        {
+            editor.Open(args[0]);
+        }
+
+        // The guidance message at the bottom
+        editor.SetStatusMessage("HELP: "
+            + "Ctrl-S: Save"
+            + "Ctrl-Q: Quit"
+            + "Ctrl-F: Find"
+        );
+
+        // Refresh the screen and process non-writing operations
+        while (true)
+        {
+            editor.RefreshScreen();
+            editor.ProcessKeypress();
+        }
     }
 }
